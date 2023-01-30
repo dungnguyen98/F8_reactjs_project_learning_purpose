703d4d6a2792bc8f60de6f77799c6ed81571f448
dover <dovername@gmail.com>
2023-01-30 14:05:38 +0700

    add usememo

--- usememo_example/src/App.js
+++ usememo_example/src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useMemo, useRef } from 'react'
 
+// useMemo dùng khi render nhiều lần một đoạn code nhưng không cần thiết
+//
 function App() {
+  const [name, setName] = useState('')
+  const [price, setPrice] = useState('')
+  const [products, setProducts] = useState([])
+
+
+  const nameRef = useRef()
+
+  const handleSubmit = () => {
+    setProducts([...products, {
+      name,
+      price: +price
+    }])
+
+    setName('')
+    setPrice('')
+    nameRef.current.focus()
+    // console.log(nameRef)
+  }
+
+  // đoạn code render nhiều lần ko cần thiết: mỗi lần gõ chữ vào ô input thì hàm này render một lần, mỗi lần render thì total được tính toán lại một lần.
+  // const total = products.reduce((result, prod) => {
+  //   console.log('re-calculate')
+  //   return result + prod.price
+  // }, 0)
+
+  const total = useMemo(() => {
+    const rs = products.reduce((result, prod) => {
+        console.log('re-calculate')
+        return result + prod.price
+      }, 0)
+    
+      return rs
+  }, [products])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input 
+        value={name}
+        placeholder="Enter name..."
+        ref={nameRef}
+        onChange={e => setName(e.target.value)}
+      />
+      <br/>
+      <input 
+        value={price}
+        placeholder="Enter price..."
+        onChange={e => setPrice(e.target.value)}
+      />
+      <br />
+      <button onClick={handleSubmit}>Add</button>
+      <br />
+      Total: {total}
+      <ul>
+        {products.map((product, index) => (
+          <li key={index}>{product.name} - {product.price}</li>
+        ))}
+      </ul>
     </div>
   );
 }
