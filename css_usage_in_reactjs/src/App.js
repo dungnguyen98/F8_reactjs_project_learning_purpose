4ad4b72495cff91274d0c72e5f4caf0127dc7c8c
dover <dovername@gmail.com>
2023-01-31 09:56:51 +0700

    add all

--- css_usage_in_reactjs/src/App.js
+++ css_usage_in_reactjs/src/App.js
@@ -1,9 +1,19 @@
 import "./App.css"
+import Heading from './components/Heading'
+import Paragraph from './components/Paragraph'
+import GlobalStyle from "./components/GlobalStyle";
 
 function App() {
   return (
     <div style={{ padding: '0 32px' }}>
-      <h1 className="heading">CSS</h1>
+      <GlobalStyle>
+        <Heading/>
+        <Paragraph/>
+        <div className="d-flex">
+          <div>Item 1</div>
+          <div>Item 2</div>
+        </div>
+      </GlobalStyle>
     </div>
   );
 }
---
+++ css_usage_in_reactjs/src/components/Paragraph/index.js
@@ -0,0 +1,11 @@
+import styles from './Paragraph.module.css'
+
+console.log(styles)
+
+function Paragraph() {
+    return (
+        <p className={styles.paragraph}>Paragraph</p>
+    )
+}
+
+export default Paragraph
